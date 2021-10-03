823c4fa120912ae6ad348b79dbb9fbb8ff49b082
uuronn <uuronprogram@gmail.com>
2021-10-04 01:11:57 +0900

    add start-component

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
+    <VStart/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import VStart from '~/components/domain/home/VStart.vue'
 
 export default Vue.extend({
+  components: {
+    VStart,
+  }
 })
 </script>
 
