fa3714fd643ceb851c68b9e2a8d998f87d8d75ed
uuronn <uuronprogram@gmail.com>
2021-12-06 16:46:54 +0900

    お題を別ファイルに保管

--- components/domain/home/VStart.vue
+++ components/domain/home/VStart.vue
@@ -27,18 +27,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-    }
-  },
-  methods: {
-    
-  }
-}
-</script>
-
 <style lang="scss" scoped>
 .start {
   text-align: center;
